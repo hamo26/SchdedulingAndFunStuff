865c0c45917b0c513421991eaf630efcf81f57d3
Justin Li <cclinus@ubuntu.(none)>
2012-09-28 11:12:03 -0700

    task gen

---
+++ 494TaskGenerator/t2.cpp
@@ -0,0 +1,136 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// Generate a random number between a and b
+float RandomFloat(float a, float b) {
+    float random = ((float) rand()) / (float) RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
+
+// Generate N-1 rands between 0 and U
+void genRand(vector<double> &randVector, int N, double U){
+    for(int i = 0; i < N-1; i++){
+        double randNo = RandomFloat(0, U);
+        randVector.push_back(randNo);
+    }
+    randVector.push_back(0);
+    randVector.push_back(1);
+}
+
+
+vector<double> merge(vector<double> leftList, vector<double> rightList) {
+
+    // Merge sort based on wikipedia pseudocode
+    // http://en.wikipedia.org/wiki/Merge_sort           
+
+    vector<double> sortedVector;
+
+    while (leftList.size() > 0 or rightList.size() > 0){
+        if (leftList.size() > 0 and rightList.size() > 0){
+            if (leftList.at(0) <= rightList.at(0)){ 
+                sortedVector.insert(sortedVector.end(), leftList.at(0));
+                leftList.erase(leftList.begin());
+            }
+            else{
+                sortedVector.insert(sortedVector.end(), rightList.at(0));
+                rightList.erase(rightList.begin());
+            }
+        }
+        else if (leftList.size() > 0){
+            sortedVector.insert(sortedVector.end(), leftList.at(0));
+            leftList.erase(leftList.begin());
+        }
+        else if (rightList.size() > 0){
+            sortedVector.insert(sortedVector.end(), rightList.at(0));
+            rightList.erase(rightList.begin());
+        }
+    }
+
+    return sortedVector;              
+}
+
+vector<double> mergeSort(vector<double> unsortedList) {
+
+    // Merge sort based on wikipedia pseudocode
+    // http://en.wikipedia.org/wiki/Merge_sort
+
+    if (unsortedList.size() <= 1)
+        return unsortedList;
+    // if unsortedList size is 1, means it is sorted and return 
+
+    // else sort               
+    // begin by splitting the list into 2 sublists
+
+    vector<double> leftList, rightList;
+    int middleValue = (unsortedList.size() / 2);
+
+    for(int i = 0; i < middleValue; i++){
+        leftList.insert(leftList.end(), unsortedList.at(i));
+
+    }
+
+    for(int i = middleValue; i< unsortedList.size(); i++){
+        rightList.insert(rightList.end(), unsortedList.at(i));
+
+    }
+
+    // keep calling mergeSort() to further split each sublist until size is 1 for sublist
+    leftList = mergeSort(leftList);
+    rightList = mergeSort(rightList);
+
+    // merge the sublists and return the resulting merged sublist
+    return merge(leftList, rightList);
+}
+
+void subRand(vector<double> &randVector, vector<double> &uVector){
+    for( int i = 0; i < randVector.size() - 1; i++){
+        uVector.insert(uVector.end(), (randVector.at(i+1) - randVector.at(i)));
+    }
+}
+
+void genPVector(vector<double> &pVector, int N){
+    for(int i = 0; i < N; i++){
+        double randNo = RandomFloat(1, 6);
+        randNo = pow10(randNo);
+        pVector.push_back(randNo);
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    double U = strtod(argv[1], NULL);
+    int N = strtod(argv[2], NULL);
+
+    vector<double> randVector;
+    vector<double> uVector;
+    vector<double> pVector;
+
+    // Generate uVector
+    genRand(randVector, N, U);
+
+    vector<double> sortedRandVector = mergeSort(randVector);
+    subRand(sortedRandVector, uVector);
+
+    // Generate pVector
+    genPVector(pVector, N);
+
+    cout << "<begin task set>\n";
+    for (int i = 0; i < N; i++){
+        double deadline = pVector.at(i);
+        double executionTime = uVector.at(i) * deadline;
+        cout << executionTime << " " << deadline << " " << deadline << "\n";
+    }
+    cout << "<end task set>\n";
+
+    return EXIT_SUCCESS;
+} 
