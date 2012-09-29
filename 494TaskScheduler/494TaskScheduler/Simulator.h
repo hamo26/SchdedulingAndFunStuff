f446b20c52bcadcb1bad56721ef95f121e4ba08b
Cary <melstrom@gmail.com>
2012-09-29 15:59:47 -0700

    More syntax fixes

--- 494TaskScheduler/494TaskScheduler/Simulator.h
+++ 494TaskScheduler/494TaskScheduler/Simulator.h
@@ -16,16 +16,15 @@ class Simulator
 {
 private:
 	TaskSet v_ts;
+
 public:
-	Simulator(TaskSet ts);
+	Simulator(vector<Task> ts);
 
 	bool RM();
 	bool SJF();
 	bool MUF();
 	queue<Task> addToWait(queue<Task> waitQueue, Task t);
 	int checkNewArrivals(int time, queue<Task> waitQueue);
-	//bool Simulator::addNewTaskOnPeriod(int time, Task t);
-
 
 };
 
