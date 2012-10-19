aa6cbbedf10dd922bdbdf39137c7248f1b59f1d2
hamo26 <hamo.ubc@gmail.com>
2012-10-18 19:25:38 -0700

    Final changes to main file for edf

--- nanork/task2/nano-RK/edfTest/main.c
+++ nanork/task2/nano-RK/edfTest/main.c
@@ -8,26 +8,26 @@
 #include <nrk_stack_check.h>
 #include <nrk_stats.h>
 
-#define CHECK_PERIOD    200
-#define LEEWAY_FACTOR   4/5
-
-#define TASK(n, task_period, task_execution)                                \
+/*
+To stay inline (no pun intended) with the nano-rk direction of using guards for 
+optimization and potential performance, we use macros rather than methods.
+We could have created a method, but the call may be expensive and impede the performance of the kernel.
+*/
+#define TASK(n, taskPeriod, taskExecution)                                \
     void task_##n##_func()                                                  \
     {                                                                       \
                                                                             \
         while (1)                                                           \
         {									\
-	nrk_wait_until_next_period();	    \
-	printf("Beginning task %d\n", n);                                	\
+			nrk_wait_until_next_period();	    \
+			printf("Running task..%d\n", n);                                	\
         }                                                                   \
     }                                                                       \
     NRK_STK stack_##n[NRK_APP_STACKSIZE];                                   \
     nrk_task_type task_##n;                                                 \
     uint32_t task_##n##_period = task_period;                               \
     uint32_t task_##n##_execution = task_execution;                         \
-
-#define ACTIVATE_TASK(n)                                                    \
-    task_##n.prio = 10 - n;                                                      \
+	task_##n.prio = 10 - n;                                                      \
     task_##n.FirstActivation = TRUE;                                        \
     task_##n.Type = BASIC_TASK;                                             \
     task_##n.SchType = PREEMPTIVE;                                          \
@@ -37,13 +37,17 @@
     task_##n.cpu_reserve.nano_secs = 0;                                     \
     task_##n.offset.secs = 0;                                               \
     task_##n.offset.nano_secs = 0;                                          \
+    
+/*Initialize the task*/
+#define INITIALIZE_TASK(n)                                                    \
     nrk_task_set_entry_function(&task_##n, task_##n##_func);                \
     nrk_task_set_stk(&task_##n, stack_##n, NRK_APP_STACKSIZE);              \
     nrk_activate_task(&task_##n)
 
+//"Instantiate" tasks.
 TASK(1, 4, 2);
 TASK(2, 7, 3);
-//TASK(3, 8, 3);
+
 
 int main ()
 {
@@ -52,16 +56,11 @@ int main ()
 
     nrk_init();
 
-    nrk_led_clr(ORANGE_LED);
-    nrk_led_clr(BLUE_LED);
-    nrk_led_clr(GREEN_LED);
-    nrk_led_clr(RED_LED);
-
     nrk_time_set(0,0);
-
-    ACTIVATE_TASK(1);
-    ACTIVATE_TASK(2);
- //   ACTIVATE_TASK(3);
+	
+	//Initialize tasks 
+    INITIALIZE_TASK(1);
+    INITIALIZE_TASK(2);
 
     nrk_start();
 
