a1953c7047d5dc5eb5ef8d600d7923f4201625c0
hamid <hamo26@interchange.ubc.ca>
2012-11-27 11:44:35 -0800

    Attempting implementation of switch with buffer.

---
+++ switch/task1b/switch_with_buffer_attempt.c
@@ -0,0 +1,130 @@
+/*******************************************************
+ *
+ *  Switch Routines for EECE 494, Assignment 2.
+ *
+ *  Created by _________, University of British Columbia
+ *
+ *  This is where you will put your routines to implement
+ *  the switch routines.  You should use the headers as
+ *  supplied.  All your code for Assignment 2 will go in this
+ *  file (and possibly in switch.h)
+ *
+ ******************************************************/
+
+#include "defs.h"
+
+void *switch_thread_routine(void *arg)
+{
+    pthread_t in_port_thread; 
+	pthread_t out_port_thread; 
+	void *read_in_port_packet();
+	void *read_out_port_packet();
+	pthread_create(&in_port_thread,NULL,read_in_port_packet,NULL);
+	pthread_create(&out_port_thread,NULL,read_out_port_packet,NULL);
+	pthread_join(in_port_thread,NULL);
+	pthread_exit(NULL);
+}
+
+void forward_packet_to_port(packet_t packet) {
+    //This should be thread safe because only one thread deals with an output port.
+    int dest_port;	
+    dest_port = get_port_from_packet(&packet);
+    std::cout<<"dest_port:"<<dest_port<<"\n";
+    while(out_port[dest_port].flag == TRUE) { }
+	pthread_mutex_lock(&out_port[dest_port].mutex);
+	out_port[dest_port].packet = packet;
+	pthread_mutex_unlock(&out_port[dest_port].mutex);
+	out_port[dest_port].flag = TRUE;
+}
+
+/* Gets a port number from a packet*/
+int get_port_from_packet(packet_t* packet) {
+    return cam_lookup_address(&(packet->address));
+}
+
+void add_packet_to_buffer(packet_t packet){
+  network_buffer[rear] = packet;
+  rear = (rear+1) % SIZE;
+  size++;
+}
+
+packet_t get_packet_from_buffer(){
+  packet_t packet;
+  packet = network_buffer[front];
+  front= (front+1) % SIZE;
+  size--;
+  return packet;
+}
+ 
+void *read_in_port_packet()
+{
+	while(1){
+		int i;
+		port_t current_input_port;			
+	
+		printf( "DIE:%d\n", die_flag );
+
+		if (die_flag == 0) { 
+			std::cout<<"DIE is true now \n";
+			pthread_exit(NULL);
+		}else{
+			std::cout<<"NOT DIE"<<"\n";
+		}
+
+		for (i = 0; i < 4; i++) {
+			current_input_port = in_port[i];
+			if (current_input_port.flag) {
+				pthread_mutex_lock(&current_input_port.mutex);
+				pthread_mutex_lock(&region_mutex);
+				if (size == SIZE) {	
+					pthread_cond_wait(&space_available,&region_mutex);
+				}
+				add_buffer(packet);
+				pthread_cond_signal(&data_available);
+				pthread_mutex_unlock(&region_mutex);
+				//Add logic here to read from network port.
+				pthread_mutex_unlock(&current_input_port.mutex);
+				current_input_port.flag = FALSE;
+			}
+		}
+
+    }
+}
+
+void *read_out_port_packet()
+{
+   packet_t packet;
+   while (1) {
+	   if (size == 0) {
+		   pthread_cond_wait(&data_available,&region_mutex); 
+	   }
+	   packet = get_packet_from_buffer();
+	   forward_packet_to_port(packet);
+	   pthread_cond_signal(&space_available);
+	}
+}
+
+void switch_init()
+{
+    /* Add your code here.  At the very least, you probably
+       want to include a call to cam_init() */
+    /* Initalize the routing table by calling your routine */
+
+    cam_init();
+
+}
+
+void switch_add_entry(ip_address_t *address,
+	int port)
+{
+    /* Add your code here.  It might be as simple as adding
+       a call to cam_add_entry() */
+
+    cam_add_entry(address,port);
+}
+
+void switch_free()
+{
+    /* add your code (if any) here */
+    cam_free();
+}
