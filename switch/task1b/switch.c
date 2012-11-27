11dec24045c768acc4450c0c81a5905e7f3cae6c
Justin Li <cclinus@cclinus-VGN-SZ422-B.(none)>
2012-11-26 22:45:02 -0800

    23% dropped

--- switch/task1b/switch.c
+++ switch/task1b/switch.c
@@ -15,62 +15,69 @@
 
 void *switch_thread_routine(void *arg)
 {
-   /* Add your code here */
-  //iterate over all input ports.
-  int i;
-  port_t current_input_port;			
-  for (i = 0; i < 4; i++) {
-  	if (die == TRUE) { return NULL; }
-	current_input_port = in_port[i];
-	if (current_input_port.flag) {
+    /* Add your code here */
+    //iterate over all input ports.
+    while(1){
+	int i;
+	port_t current_input_port;			
+	for (i = 0; i < 4; i++) {
+	    if (die == true) { 
+		std::cout<<"DIE is true now \n";
+		return NULL; 
+	    }
+
+	    current_input_port = in_port[i];
+	    if (current_input_port.flag) {
 		pthread_mutex_lock(&current_input_port.mutex);
 		forward_packet_to_port(current_input_port.packet);
-		current_input_port.flag = false;
 		pthread_mutex_unlock(&current_input_port.mutex);
+		current_input_port.flag = false;
+	    }
 	}
-  }
-  usleep(THREAD_SLEEP_TIME);				   
+	usleep(100000);
+    }
 }
 
 void forward_packet_to_port(packet_t packet) {
-//This should be thread safe because only one thread deals with an output port.
-	int dest_port;	
-	dest_port = get_port_from_packet(&packet);
-	if (!out_port[dest_port].flag) {
-		pthread_mutex_lock(&out_port[dest_port].mutex);
-		out_port[dest_port].packet = packet;
-		out_port[dest_port].flag = true;
-		pthread_mutex_unlock(&out_port[dest_port].mutex);
-	} 
-	return;
+    //This should be thread safe because only one thread deals with an output port.
+    int dest_port;	
+    dest_port = get_port_from_packet(&packet);
+    std::cout<<"dest_port:"<<dest_port<<"\n";
+    if (!out_port[dest_port].flag) {
+	pthread_mutex_lock(&out_port[dest_port].mutex);
+	out_port[dest_port].packet = packet;
+	pthread_mutex_unlock(&out_port[dest_port].mutex);
+	out_port[dest_port].flag = true;
+    } 
+    return;
 }
 
 /* Gets a port number from a packet*/
 int get_port_from_packet(packet_t* packet) {
-	return cam_lookup_address(&(packet->address));
+    return cam_lookup_address(&(packet->address));
 }
 
 void switch_init()
 {
-   /* Add your code here.  At the very least, you probably
-      want to include a call to cam_init() */
-	/* Initalize the routing table by calling your routine */
+    /* Add your code here.  At the very least, you probably
+       want to include a call to cam_init() */
+    /* Initalize the routing table by calling your routine */
 
     cam_init();
 
 }
 
 void switch_add_entry(ip_address_t *address,
-                      int port)
+	int port)
 {
-   /* Add your code here.  It might be as simple as adding
-      a call to cam_add_entry() */
+    /* Add your code here.  It might be as simple as adding
+       a call to cam_add_entry() */
 
-        cam_add_entry(address,port);
+    cam_add_entry(address,port);
 }
 
 void switch_free()
 {
-   /* add your code (if any) here */
-	cam_free();
+    /* add your code (if any) here */
+    cam_free();
 }
